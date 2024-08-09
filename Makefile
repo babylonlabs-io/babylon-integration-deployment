a99b6d4aa2b38967a8aae19d3fdc2b351b2d9c47
bap2pecs <bap2pecs@proton.me>
2024-08-08 22:33:18 -0400

    wip

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
 start-deployment-btc-staking-integration-bitcoind:
-	$(MAKE) -C $(CURDIR)/deployments/btc-staking-integration-bitcoind \
+	@$(MAKE) -C $(CURDIR)/deployments/btc-staking-integration-bitcoind \
 		start-deployment-btc-staking-integration-bitcoind
 
 start-deployment-btc-staking-integration-bitcoind-demo:
-	$(MAKE) -C $(CURDIR)/deployments/btc-staking-integration-bitcoind \
+	@$(MAKE) -C $(CURDIR)/deployments/btc-staking-integration-bitcoind \
 		NUM_VALIDATORS=${NUM_VALIDATORS} \
 		start-deployment-btc-staking-integration-bitcoind-demo
 
 stop-deployment-btc-staking-integration-bitcoind:
-	$(MAKE) -C $(CURDIR)/deployments/btc-staking-integration-bitcoind \
+	@$(MAKE) -C $(CURDIR)/deployments/btc-staking-integration-bitcoind \
 		stop-deployment-btc-staking-integration-bitcoind
+
+start-deployment-finality-gadget-integration-op-l2-demo:
+	@$(MAKE) -C $(CURDIR)/deployments/finality-gadget-integration-op-l2 \
+		demo-test
--- deployments/finality-gadget-integration-op-l2/deploy-cw-contract.sh
+++ deployments/finality-gadget-integration-op-l2/deploy-cw-contract.sh
@@ -1,61 +1,91 @@
 #!/bin/bash
 set -euo pipefail
 
-# Checks if at least two arguments are provided: consumer_id and activated_height
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <consumer_id> <activated_height> [admin]"
+### Checks if at least three arguments are provided: consumer_id and activated_height
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <consumer_id> <activated_height> <admin>"
     exit 1
 fi
-
-HOME_DIR="/babylondhome"
-KEY_NAME="test-spending-key"
-CHAIN_ID="chain-test"
-WASM_FILE="artifacts/op_finality_gadget.wasm"
-LABEL="op_finality_gadget"
-
 CONSUMER_ID="$1"
 ACTIVATED_HEIGHT="$2"
-# Sets the ADMIN to the third argument if provided, otherwise uses the DEPLOYER_ADDRESS
-DEPLOYER_ADDRESS=$(babylond keys show -a $KEY_NAME --keyring-backend test --home $HOME_DIR)
-ADMIN="${3:-$DEPLOYER_ADDRESS}"
+ADMIN="$3"
+echo "CONSUMER_ID: $1"
+echo "ACTIVATED_HEIGHT: $2"
+echo "ADMIN: $3"
+echo
 
-echo "Upload the CW contract code"
-TX_HASH=$(babylond tx wasm store $WASM_FILE \
-        --from $DEPLOYER_ADDRESS \
-        --keyring-backend test \
-        --home $HOME_DIR \
-        --chain-id $CHAIN_ID \
-        --gas-prices 0.2ubbn --gas auto --gas-adjustment 1.3 \
-        -o json -y | jq -r '.txhash')
+### Define constants
+HOME_DIR="/babylondhome"
+TEST_SPENDING_KEY_NAME="test-spending-key"
+CHAIN_ID="chain-test"
+WASM_FILE_LOCAL="artifacts/op_finality_gadget.wasm"
+WASM_FILE_CONTAINER="/home/babylon/op_finality_gadget.wasm"
+LABEL="op_finality_gadget"
+# Get the container ID of babylondnode0
+CONTAINER_ID=$(docker ps -qf "name=babylondnode0")
 
-sleep 2
+### Store the CW contract code
+# Copy the wasm file into the container
+docker cp $WASM_FILE_LOCAL $CONTAINER_ID:$WASM_FILE_CONTAINER
+echo
+echo "Storing the CW contract code..."
+STORE_CODE_TX_HASH=$(docker exec babylondnode0 /bin/sh -c "
+    /bin/babylond tx wasm store $WASM_FILE_CONTAINER \
+    --home $HOME_DIR \
+    --from $TEST_SPENDING_KEY_NAME \
+    --chain-id $CHAIN_ID \
+    --keyring-backend test \
+    --gas-prices 0.2ubbn \
+    --gas auto \
+    --gas-adjustment 1.3 \
+    -o json -y" | jq -r '.txhash')
+echo "STORE_CODE_TX_HASH: $STORE_CODE_TX_HASH"
+echo
+# TODO: add a for loop to check if the code is stored
+sleep 7
 
-echo "Query the code_id with tx hash: $TX_HASH"
-CODE_ID=$(babylond query tx $TX_HASH \
-        --home $HOME_DIR \
-        --chain-id $CHAIN_ID \
-        -o json | jq -r '.events[] | select(.type == "store_code") | .attributes[] | select(.key == "code_id") | .value')
+### Query the code_id using the tx hash
+echo "Querying the code_id using the tx hash..."
+CODE_ID=$(docker exec babylondnode0 /bin/sh -c "
+    /bin/babylond query tx $STORE_CODE_TX_HASH \
+    --home $HOME_DIR \
+    --chain-id $CHAIN_ID \
+    -o json" \
+    | jq -r '.events[] | select(.type == "store_code") | .attributes[] | select(.key == "code_id") | .value')
+echo "CODE_ID: $CODE_ID"
+echo
 
+
+### Instantiate the CW contract
 JSON_VALUE=$(printf '{"admin":"%s","consumer_id":"%s","activated_height":%d,"is_enabled":true}' "$ADMIN" "$CONSUMER_ID" "$ACTIVATED_HEIGHT")
-echo "Instantiate the CW contract with code_id: $CODE_ID and the value: $JSON_VALUE"
-babylond tx wasm instantiate $CODE_ID $JSON_VALUE \
-    --label $LABEL --admin $ADMIN \
-    --from $DEPLOYER_ADDRESS \
+echo "Instantiating the CW contract with arguments: $JSON_VALUE ..."
+CREATE_CONTRACT_TX_HASH=$(docker exec babylondnode0 /bin/sh -c "
+    /bin/babylond tx wasm instantiate $CODE_ID '$JSON_VALUE' \
+    --label $LABEL \
+    --admin $ADMIN \
+    --from $TEST_SPENDING_KEY_NAME \
     --keyring-backend test \
     --home $HOME_DIR \
     --chain-id $CHAIN_ID \
     --gas-prices 0.2ubbn --gas auto --gas-adjustment 1.3 \
-    -o json -y
+    -o json -y" | jq -r '.txhash')
+echo "CREATE_CONTRACT_TX_HASH: $CREATE_CONTRACT_TX_HASH"
+echo
+sleep 5
 
-sleep 2
 
+### Query the CW contract config to verify the CW contract is instantiated correctly
 echo "Query the CW contract config to verify the CW contract is instantiated correctly"
-CONTRACT_ADDRESS=$(babylond query wasm list-contract-by-code $CODE_ID \
-        --chain-id $CHAIN_ID \
-        --home $HOME_DIR \
-        -o json | jq -r '.contracts[]')
+CONTRACT_ADDRESS=$(docker exec babylondnode0 /bin/sh -c "
+    /bin/babylond query wasm list-contract-by-code $CODE_ID \
+    --chain-id $CHAIN_ID \
+    --home $HOME_DIR \
+    -o json" | jq -r '.contracts[]')
+echo "Contract address: $CONTRACT_ADDRESS"
 QUERY_CONFIG='{"config":{}}'
-babylond query wasm contract-state smart $CONTRACT_ADDRESS $QUERY_CONFIG \
+CONTRACT_CONFIG=$(docker exec babylondnode0 /bin/sh -c "
+    /bin/babylond query wasm contract-state smart $CONTRACT_ADDRESS '$QUERY_CONFIG' \
     --home $HOME_DIR \
     --chain-id $CHAIN_ID \
-    -o json
+    -o json")
+echo "Contract config: $CONTRACT_CONFIG"
