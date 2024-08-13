29d776460a822c9d78d09a674d71e1342a49639a
lesterli <fulfilself@gmail.com>
2024-08-13 21:19:35 +0800

    fix: upgrade optimism

--- deployments/finality-gadget-integration-op-l2/prepare-op-devnet.sh
+++ deployments/finality-gadget-integration-op-l2/prepare-op-devnet.sh
@@ -1,48 +1,34 @@
 #!/bin/bash
 set -euo pipefail
 
+OP_DIR=$1
+echo "OP_DIR: $OP_DIR"
+echo
+
+# Read foundry and just version from versions.json
+FOUNDRY_VERSION=$(jq -r .foundry < "$OP_DIR"/versions.json)
+JUST_VERSION=$(jq -r .just < "$OP_DIR"/versions.json)
+
 # Function to check if a command exists
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# Install NVM if not already installed
-if [ -s "$NVM_DIR/nvm.sh" ]; then
-    echo "NVM is already installed."
+# Install just if not already installed
+if ! command_exists just || [[ "$(just --version 2>/dev/null | awk '{print $2}' || echo "")" != "${JUST_VERSION}" ]]; then
+    echo "Installing just..."
+    mkdir -p $HOME/.just
+    curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to $HOME/.just --tag "${JUST_VERSION}"
+    export PATH=$PATH:$HOME/.just
 else
-    echo "Installing NVM..."
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
+    echo "just is already installed."
 fi
-. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm --version
-echo
-
-# Install Node if not already installed
-if ! command_exists node; then
-    echo "Installing Node..."
-    nvm install 20
-    nvm use 20
-else
-    echo "Node is already installed. Version: $(node --version)"
-fi
-node --version
-npm --version
-echo
-
-# Install pnpm if not already installed
-if ! command_exists pnpm; then
-    echo "Installing pnpm..."
-    npm install pnpm --global
-else
-    echo "pnpm is already installed. Version: $(pnpm --version)"
-fi
-pnpm --version
+just --version
 echo
 
 # Check if Go is already installed
 if command_exists go; then
-    echo "Go is already installed. Version: $(go version)"
+    echo "Go is already installed."
 else
     # Install Golang
     echo "Installing Go..."
@@ -50,35 +36,35 @@ else
     tar -xzf go1.21.5.linux-amd64.tar.gz -C $HOME
     export GOROOT=$HOME/go
     export PATH=$PATH:$GOROOT/bin
-    go version
 fi
+go version
 echo
 
 # Install Foundry if not already installed
-if ! command_exists forge; then
+if ! command_exists forge || [[ "$(forge --version 2>/dev/null | sed -n 's/.*(\([a-f0-9]\{7\}\).*/\1/p' || echo "")" != "${FOUNDRY_VERSION:0:7}" ]]; then
     echo "Installing Foundry..."
+    mkdir -p $HOME/.foundry/bin
     if [[ "$(uname)" == "Linux" ]]; then
-        curl -L https://github.com/foundry-rs/foundry/releases/download/nightly-$FOUNDRY_VERSION/foundry_nightly_linux_amd64.tar.gz | tar xvzf -
+        curl -L https://github.com/foundry-rs/foundry/releases/download/nightly-$FOUNDRY_VERSION/foundry_nightly_linux_amd64.tar.gz | tar xvzf - -C $HOME/.foundry/bin
     elif [[ "$(uname)" == "Darwin" ]]; then # for MacOS
-        curl -L https://github.com/foundry-rs/foundry/releases/download/nightly-$FOUNDRY_VERSION/foundry_nightly_darwin_amd64.tar.gz | tar xvzf -
+        curl -L https://github.com/foundry-rs/foundry/releases/download/nightly-$FOUNDRY_VERSION/foundry_nightly_darwin_amd64.tar.gz | tar xvzf - -C $HOME/.foundry/bin
     else
         echo "unsupported $(uname) system"
         exit 1
     fi
-    mv forge cast anvil chisel $HOME/.foundry/bin
     export PATH=$HOME/.foundry/bin:$PATH
 else
-    echo "Foundry is already installed. Version: $(forge --version)"
+    echo "Foundry is already installed"
 fi
 forge --version
 echo
 
-OP_DIR=$1
-echo "OP_DIR: $OP_DIR"
-cd $OP_DIR
+# Go to the OP contracts directory
+OP_CONTRACTS_DIR=$OP_DIR/packages/contracts-bedrock
+echo "OP_CONTRACTS_DIR: $OP_CONTRACTS_DIR"
+cd $OP_CONTRACTS_DIR
 
-# Install dependencies
-echo "Installing dependencies..."
-pnpm install
-pnpm build
+# Install contracts dependencies
+echo "Installing the dependencies for the smart contracts..."
+just install
 echo
