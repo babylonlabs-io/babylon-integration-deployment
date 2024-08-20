fbdc71231cb0646a28067fc7d747a813e4f8a758
lesterli <fulfilself@gmail.com>
2024-08-20 21:01:43 +0800

    update verify script

--- deployments/finality-gadget-integration-op-l2/verify-babylon-finality.sh
+++ deployments/finality-gadget-integration-op-l2/verify-babylon-finality.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "Wait a few minutes to verify Babylon finality gadget integration..."
+echo
+
 FINALITY_GADGET_DIR=$1
 PROTO_FILE="$FINALITY_GADGET_DIR/proto/finalitygadget.proto"
 PROTO_PATH="$FINALITY_GADGET_DIR/proto"
 GRPC_SERVER="localhost:50051"
 
-# Call the QueryLatestFinalizedBlock method
-FG_LATEST_FINALIZED_BLOCK=$(grpcurl -proto $PROTO_FILE \
+echo "Fetching the OP L2 sync status..."
+while true; do
+  OP_L2_SYNC_STATUS=$(curl -s -X POST -H "Content-Type: application/json" \
+      --data '{"jsonrpc":"2.0","method":"optimism_syncStatus","params":[],"id":1}' \
+      http://localhost:7545)
+  OP_L2_SYNC_STATUS_FINALIZED_L2_HASH=$(echo $OP_L2_SYNC_STATUS | jq -r '.result.finalized_l2.hash')
+  OP_L2_SYNC_STATUS_FINALIZED_L2_NUMBER=$(echo $OP_L2_SYNC_STATUS | jq -r '.result.finalized_l2.number')
+  OP_L2_SYNC_STATUS_FINALIZED_L2_TIMESTAMP=$(echo $OP_L2_SYNC_STATUS | jq -r '.result.finalized_l2.timestamp')
+
+  echo "OP L2 sync status - finalized L2 block number: $OP_L2_SYNC_STATUS_FINALIZED_L2_NUMBER"
+
+  if [ "$OP_L2_SYNC_STATUS_FINALIZED_L2_NUMBER" -gt 50 ]; then
+      echo "It is time to verify the latest finalized block"
+      break
+  else
+      sleep 15
+  fi
+done
+sleep 5
+echo
+
+echo "Querying if the latest finalized block is Babylon finalized..."
+echo "OP L2 block number: $OP_L2_SYNC_STATUS_FINALIZED_L2_NUMBER"
+echo "OP L2 block hash: $OP_L2_SYNC_STATUS_FINALIZED_L2_HASH"
+echo "OP L2 block timestamp: $OP_L2_SYNC_STATUS_FINALIZED_L2_TIMESTAMP"
+BLOCK_IS_BABYLON_FINALIZED=$(grpcurl -proto $PROTO_FILE \
   -import-path $PROTO_PATH \
-  -plaintext -d '{}' \
+  -plaintext \
+  -d '{
+    "block": {
+      "block_hash": "'"$OP_L2_SYNC_STATUS_FINALIZED_L2_HASH"'",
+      "block_height": '"$OP_L2_SYNC_STATUS_FINALIZED_L2_NUMBER"',
+      "block_timestamp": '"$OP_L2_SYNC_STATUS_FINALIZED_L2_TIMESTAMP"'
+    }
+  }' \
   $GRPC_SERVER \
-  proto.FinalityGadget/QueryLatestFinalizedBlock)
-FG_LATEST_FINALIZED_BLOCK_HASH=$(echo $FG_LATEST_FINALIZED_BLOCK | jq -r '.block.blockHash')
-FG_LATEST_FINALIZED_BLOCK_HEIGHT=$(echo $FG_LATEST_FINALIZED_BLOCK | jq -r '.block.blockHeight')
-FG_LATEST_FINALIZED_BLOCK_TIMESTAMP=$(echo $FG_LATEST_FINALIZED_BLOCK | jq -r '.block.blockTimestamp')
+  proto.FinalityGadget/QueryIsBlockBabylonFinalized | jq -r '.isFinalized')
 echo
 
-# Fetch the OP L2 sync status
-OP_L2_SYNC_STATUS=$(curl -s -X POST -H "Content-Type: application/json" \
-    --data '{"jsonrpc":"2.0","method":"optimism_syncStatus","params":[],"id":1}' \
-    http://localhost:7545)
-OP_L2_SYNC_STATUS_FINALIZED_L2_HASH=$(echo $OP_L2_SYNC_STATUS | jq -r '.result.finalized_l2.hash')
-OP_L2_SYNC_STATUS_FINALIZED_L2_NUMBER=$(echo $OP_L2_SYNC_STATUS | jq -r '.result.finalized_l2.number')
-OP_L2_SYNC_STATUS_FINALIZED_L2_TIMESTAMP=$(echo $OP_L2_SYNC_STATUS | jq -r '.result.finalized_l2.timestamp')
-echo
-
-# Verify and print the results
-if [ "$FG_LATEST_FINALIZED_BLOCK_HASH" == "$OP_L2_SYNC_STATUS_FINALIZED_L2_HASH" ] && \
-   [ "$FG_LATEST_FINALIZED_BLOCK_HEIGHT" == "$OP_L2_SYNC_STATUS_FINALIZED_L2_NUMBER" ] && \
-   [ "$FG_LATEST_FINALIZED_BLOCK_TIMESTAMP" == "$OP_L2_SYNC_STATUS_FINALIZED_L2_TIMESTAMP" ]; then
-  echo "The latest finalized block is the same between the finality gadget and OP L2:"
-  echo "blockHash: $FG_LATEST_FINALIZED_BLOCK_HASH"
-  echo "blockHeight: $FG_LATEST_FINALIZED_BLOCK_HEIGHT"
-  echo "blockTimestamp: $FG_LATEST_FINALIZED_BLOCK_TIMESTAMP"
+if [ "$BLOCK_IS_BABYLON_FINALIZED" == "true" ]; then
+  echo "The latest finalized block $OP_L2_SYNC_STATUS_FINALIZED_L2_NUMBER is Babylon finalized"
 else
-  echo "The latest finalized block is different between the finality gadget and OP L2:"
-  echo "Finality Gadget - Hash: $FG_LATEST_FINALIZED_BLOCK_HASH, Height: $FG_LATEST_FINALIZED_BLOCK_HEIGHT, Timestamp: $FG_LATEST_FINALIZED_BLOCK_TIMESTAMP"
-  echo "OP L2 - Hash: $OP_L2_SYNC_STATUS_FINALIZED_L2_HASH, Number: $OP_L2_SYNC_STATUS_FINALIZED_L2_NUMBER, Timestamp: $OP_L2_SYNC_STATUS_FINALIZED_L2_TIMESTAMP"
+  echo "The latest finalized block $OP_L2_SYNC_STATUS_FINALIZED_L2_NUMBER is not Babylon finalized"
   exit 1
 fi
+echo
