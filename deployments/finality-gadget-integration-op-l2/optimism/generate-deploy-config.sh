05b097d56dda5d6e4ab60bdd73411aa14fde73f5
lesterli <fulfilself@gmail.com>
2024-10-04 17:54:52 +0800

    chore: use the config.sh to avoid copying codes

--- deployments/finality-gadget-integration-op-l2/optimism/generate-deploy-config.sh
+++ deployments/finality-gadget-integration-op-l2/optimism/generate-deploy-config.sh
@@ -1,32 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -euo pipefail
 
-# This script is used to generate the deployment configuration JSON file.
 OP_DIR=$1
-DEPLOY_CONFIG_PATH=${OP_DIR}/packages/contracts-bedrock/deploy-config/sepolia-devnet-${L2_CHAIN_ID}.json
+DEPLOY_CONFIG_PATH=${OP_DIR}/packages/contracts-bedrock/deploy-config
+
+# Run the Optimism Monorepo's config.sh script
+# which will generate the getting-started.json configuration file
 echo "Generating deployment configuration for OP L2..."
-
-reqenv() {
-    if [ -z "${!1}" ]; then
-        echo "Error: environment variable '$1' is undefined"
-        exit 1
-    fi
-}
-
-# Check required environment variables
-reqenv "GS_ADMIN_ADDRESS"
-reqenv "GS_BATCHER_ADDRESS"
-reqenv "GS_PROPOSER_ADDRESS"
-reqenv "GS_SEQUENCER_ADDRESS"
-reqenv "L1_RPC_URL"
-reqenv "L1_CHAIN_ID"
-reqenv "L2_CHAIN_ID"
-reqenv "L1_BLOCK_TIME"
-reqenv "L2_BLOCK_TIME"
-
-# Get the finalized block timestamp and hash
-block=$(cast block finalized --rpc-url "$L1_RPC_URL")
-timestamp=$(echo "$block" | awk '/timestamp/ { print $2 }')
-blockhash=$(echo "$block" | awk '/hash/ { print $2 }')
+${OP_DIR}/packages/contracts-bedrock/scripts/getting-started/config.sh
+# Check if the config.sh script failed
+if [ $? -ne 0 ]; then
+    echo "Failed to run config.sh script"
+    exit 1
+fi
 
 # Check optional environment variables
 DEVNET_L2OO=${DEVNET_L2OO:-true}
@@ -38,92 +24,10 @@ else
   USE_FAULT_PROOFS=true
 fi
 
-# Generate the config file
-config=$(cat << EOL
-{
-  "l1StartingBlockTag": "$blockhash",
-
-  "l1ChainID": $L1_CHAIN_ID,
-  "l2ChainID": $L2_CHAIN_ID,
-  "l2BlockTime": $L2_BLOCK_TIME,
-  "l1BlockTime": $L1_BLOCK_TIME,
-
-  "maxSequencerDrift": 600,
-  "sequencerWindowSize": 3600,
-  "channelTimeout": 300,
-
-  "p2pSequencerAddress": "$GS_SEQUENCER_ADDRESS",
-  "batchInboxAddress": "0xff511a9000000000000000000000000000000901",
-  "batchSenderAddress": "$GS_BATCHER_ADDRESS",
-
-  "l2OutputOracleSubmissionInterval": 120,
-  "l2OutputOracleStartingBlockNumber": 0,
-  "l2OutputOracleStartingTimestamp": $timestamp,
-
-  "l2OutputOracleProposer": "$GS_PROPOSER_ADDRESS",
-  "l2OutputOracleChallenger": "$GS_ADMIN_ADDRESS",
-
-  "finalizationPeriodSeconds": 12,
-
-  "proxyAdminOwner": "$GS_ADMIN_ADDRESS",
-  "baseFeeVaultRecipient": "$GS_ADMIN_ADDRESS",
-  "l1FeeVaultRecipient": "$GS_ADMIN_ADDRESS",
-  "sequencerFeeVaultRecipient": "$GS_ADMIN_ADDRESS",
-  "finalSystemOwner": "$GS_ADMIN_ADDRESS",
-  "superchainConfigGuardian": "$GS_ADMIN_ADDRESS",
-
-  "baseFeeVaultMinimumWithdrawalAmount": "0x8ac7230489e80000",
-  "l1FeeVaultMinimumWithdrawalAmount": "0x8ac7230489e80000",
-  "sequencerFeeVaultMinimumWithdrawalAmount": "0x8ac7230489e80000",
-  "baseFeeVaultWithdrawalNetwork": 0,
-  "l1FeeVaultWithdrawalNetwork": 0,
-  "sequencerFeeVaultWithdrawalNetwork": 0,
-
-  "gasPriceOracleOverhead": 0,
-  "gasPriceOracleScalar": 1000000,
-
-  "enableGovernance": true,
-  "governanceTokenSymbol": "OP",
-  "governanceTokenName": "Optimism",
-  "governanceTokenOwner": "$GS_ADMIN_ADDRESS",
-
-  "l2GenesisBlockGasLimit": "0x1c9c380",
-  "l2GenesisBlockBaseFeePerGas": "0x3b9aca00",
-  "l2GenesisRegolithTimeOffset": "0x0",
-
-  "eip1559Denominator": 50,
-  "eip1559DenominatorCanyon": 250,
-  "eip1559Elasticity": 6,
-
-  "l2GenesisEcotoneTimeOffset": "0x0",
-  "l2GenesisDeltaTimeOffset": "0x0",
-  "l2GenesisCanyonTimeOffset": "0x0",
-
-  "systemConfigStartBlock": 0,
-
-  "requiredProtocolVersion": "0x0000000000000000000000000000000000000000000000000000000000000000",
-  "recommendedProtocolVersion": "0x0000000000000000000000000000000000000000000000000000000000000000",
-
-  "faultGameAbsolutePrestate": "0x03c7ae758795765c6664a5d39bf63841c71ff191e9189522bad8ebff5d4eca98",
-  "faultGameMaxDepth": 44,
-  "faultGameClockExtension": 0,
-  "faultGameMaxClockDuration": 600,
-  "faultGameGenesisBlock": 0,
-  "faultGameGenesisOutputRoot": "0x0000000000000000000000000000000000000000000000000000000000000000",
-  "faultGameSplitDepth": 14,
-  "faultGameWithdrawalDelay": 604800,
-
-  "preimageOracleMinProposalSize": 1800000,
-  "preimageOracleChallengePeriod": 120,
-
-  "babylonFinalityGadgetRpc": "$BBN_FINALITY_GADGET_RPC",
-
-  "useFaultProofs": $USE_FAULT_PROOFS
-}
-EOL
-)
-
-# Write the config file
-echo "$config" > ${DEPLOY_CONFIG_PATH}
-echo "Deployment configuration generated at ${DEPLOY_CONFIG_PATH}"
+# Create the config file with the additional fields, e.g. useFaultProofs
+GETTING_STARTED_OUTFILE=${DEPLOY_CONFIG_PATH}/getting-started.json
+DEPLOY_CONFIG_OUTFILE=${DEPLOY_CONFIG_PATH}/sepolia-devnet-${L2_CHAIN_ID}.json
+jq ".useFaultProofs = ${USE_FAULT_PROOFS}" ${GETTING_STARTED_OUTFILE} > ${DEPLOY_CONFIG_OUTFILE}
+rm ${GETTING_STARTED_OUTFILE}
+echo "Deployment configuration generated at ${DEPLOY_CONFIG_OUTFILE}"
 echo
