8911f338106b97cee5a0844ed7e6e6d5db514902
lesterli <fulfilself@gmail.com>
2024-10-18 15:52:31 +0800

    feat: add a ci workflow to publish the bitcoinsim image

--- contrib/images/bitcoindsim/wrapper.sh
+++ contrib/images/bitcoindsim/wrapper.sh
@@ -23,7 +23,7 @@ if [[ ! -d "$BITCOIN_DATA" ]]; then
 fi
 echo "Generating bitcoin.conf file at $BITCOIN_CONF"
 if [[ "$BITCOIN_NETWORK" == "testnet" ]]; then
-  NETWORK_LABEL="test"
+  NETWORK_LABEL="test" # Testnet3
 else
   NETWORK_LABEL="$BITCOIN_NETWORK"
 fi
